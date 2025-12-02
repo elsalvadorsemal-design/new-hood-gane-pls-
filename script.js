33e51de2edbe01ad741efd24fcb8afec4c6d4cae
elsalvadorsemal-design <elsalvadorsemal@gmail.com>
2025-12-02 20:10:58 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Murdaside Website läuft");
+
+document.querySelectorAll(".buy-btn").forEach(btn => {
+    btn.addEventListener("click", () => {
+        alert("Zahlungssystem kommt später!");
+    });
+});
